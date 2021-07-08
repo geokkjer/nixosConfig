cdbee22f25f04762a918c713fcb873c4d62c5655
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2021-07-09 00:04:22 +0200

    fixed network settings on the-eschatologist

--- hosts/the-eschatologist.nix
+++ hosts/the-eschatologist.nix
@@ -27,13 +27,14 @@
 
   # Networking
   networking.useDHCP = false;
-  networking.interfaces.enp11s0.useDHCP = true;
-  networking.interfaces.enp15s0.useDHCP = true;
+  networking.interfaces.enp10s0.useDHCP = true;
+  networking.interfaces.enp9s0.useDHCP = true;
   networking.bridges = {
     "br0" = {
-      interfaces = [ "enp15s0" ];
+      interfaces = [ "enp9s0" ];
   };
   };
+  
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
    console = {
@@ -49,17 +50,9 @@
      extraGroups = [ "wheel" "libvirtd" ];
   };
 
-  
   # Allow unfree
   nixpkgs.config.allowUnfree = true;
   
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
