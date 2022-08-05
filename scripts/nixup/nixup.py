9912e527de556afea8475ad812b0ef2eb6f78244
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-08-05 11:15:42 +0200

    finished nixup python script

--- scripts/nixup/nixup.py
+++ scripts/nixup/nixup.py
@@ -2,16 +2,15 @@
 import os
 import click
 
+@click.group
+def upgrade():
+    pass
 
 @click.command()
-@click.option("boot", help="install nixos upgrades as new generation available at next boot.")
-@click.option("switch", help="install nixos upgrades as new generation available now.")
-def upgrade(boot, switch):
+def boot():
     upgradeBoot = 'sudo nixos-rebuild boot --upgrade'
-    upgradeSwitch = 'sudo nixos-rebuild switch'
     collect = 'sudo nix-collect-garbage -d'
     optimise = 'sudo nix-store --optimise'
-
     print(f"Upgrading ...")
     os.system(upgradeBoot)
     print(f"Collecting garbage ...")
@@ -19,7 +18,20 @@ def upgrade(boot, switch):
     print(f"optimising for space ...")
     os.system(optimise)
 
+@click.command()
+def switch():
+    upgradeSwitch = 'sudo nixos-rebuild switch'
+    collect = 'sudo nix-collect-garbage -d'
+    optimise = 'sudo nix-store --optimise'
+    print(f"Upgrading ...")
+    os.system(upgradeSwitch)
+    print(f"Collecting garbage ...")
+    os.system(collect)
+    print(f"optimising for space ...")
+    os.system(optimise)  
 
+upgrade.add_command(boot)
+upgrade.add_command(switch)
 
 if __name__ == '__main__':
     upgrade()
