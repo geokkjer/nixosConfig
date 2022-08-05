c97835050992faeab8180f1f56018fde4b7eab87
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-08-05 13:55:05 +0200

    added APTH variable to zshrc

--- users/geir/home.nix
+++ users/geir/home.nix
@@ -34,28 +34,31 @@ programs.zsh.enable = true;
 programs.zsh.syntaxHighlighting.enable = true;
 programs.zsh.autosuggestions.enable = true;
 programs.zsh.interactiveShellInit = ''
-   export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
-   export FZF_BASE=${pkgs.fzf}/share/fzf/
-   # Customize your oh-my-zsh options here
-   ZSH_THEME="agnoster"
-   plugins=(git fzf )
-   HISTFILESIZE=500000
-   HISTSIZE=500000
-   setopt SHARE_HISTORY
-   setopt HIST_IGNORE_ALL_DUPS
-   setopt HIST_IGNORE_DUPS
-   setopt INC_APPEND_HISTORY
-   autoload -U compinit && compinit
-   unsetopt menu_complete
-   setopt completealiases
+  export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+  export FZF_BASE=${pkgs.fzf}/share/fzf/
+  export PATH="~/.bin:$PATH"
+  # Customize your oh-my-zsh options here
+  
+  ZSH_THEME="agnoster"
+  plugins=(git fzf )
+  HISTFILESIZE=500000
+  HISTSIZE=500000
+  setopt SHARE_HISTORY
+  setopt HIST_IGNORE_ALL_DUPS
+  setopt HIST_IGNORE_DUPS
+  setopt INC_APPEND_HISTORY
+  autoload -U compinit && compinit
+  unsetopt menu_complete
+  setopt completealiases
 
-   if [ -f ~/.aliases ]; then
-     source ~/.aliases
-   fi
+  if [ -f ~/.aliases ]; then
+    source ~/.aliases
+  fi
     
    
 
-    source $ZSH/oh-my-zsh.sh
+  source $ZSH/oh-my-zsh.sh
+  
  '';
  programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 }
