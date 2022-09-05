d01e63c4ff18b0e53e60a6c284488a197d5687dc
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-09-05 10:57:50 +0200

    made zsh conf more modular

--- system/zsh.nix
+++ system/zsh.nix
@@ -1,16 +1,48 @@
 { config, pkgs, ... }:
-{  
-# Zsh and ohMyZsh
+{
+environment.systemPackages = with pkgs;
+    [
+      # sys
+      zsh
+      bash-completion 
+      zsh-completions 
+      nix-zsh-completions 
+      nix-bash-completions
+      zsh-powerlevel10k
+    ];  
 
-  programs.zsh = {    
-    enable = true;
-	enableCompletion = true;
-	syntaxHighlighting.enable = true;
-  };
-  programs.zsh.ohMyZsh = {
-	enable = true;
-	plugins = [ "git" "sudo" "docker" "kubectl" "systemd" ];
-	theme = "gentoo";
 
-  };
+
+programs.zsh.enable = true;
+programs.zsh.syntaxHighlighting.enable = true;
+programs.zsh.autosuggestions.enable = true;
+programs.zsh.interactiveShellInit = ''
+  export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh/
+  export FZF_BASE=${pkgs.fzf}/share/fzf/
+  
+
+  # Customize your oh-my-zsh options here
+  
+  ZSH_THEME="agnoster"
+  plugins=(git fzf )
+  HISTFILESIZE=50000
+  HISTSIZE=50000
+  setopt SHARE_HISTORY
+  setopt HIST_IGNORE_ALL_DUPS
+  setopt HIST_IGNORE_DUPS
+  setopt INC_APPEND_HISTORY
+  autoload -U compinit && compinit
+  unsetopt menu_complete
+  setopt completealiases
+
+  if [ -f ~/.aliases ]; then
+    source ~/.aliases
+  fi
+    
+   
+
+  source $ZSH/oh-my-zsh.sh
+  
+ '';
+ programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
 }
