22056e8b988a546307f566530d576163dce53330
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-09-05 12:10:01 +0200

    trying to make some imporvments to the install.sh script

--- hosts/the-sober-counsel.nix
+++ hosts/the-sober-counsel.nix
@@ -2,45 +2,46 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, modulesPath, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
-    [ # Include results of the hardware scan.
-      	./hardware/hardware-configuration-the-sober-counsel.nix
-      	./system/fonts.nix
-      	./system/common.nix
-      	./system/tty.nix
-      	./desktop/apps.nix
-      	./desktop/browsers.nix
-      	./desktop/gnome.nix
-      	./desktop/steam.nix
-        ./desktop/audio-software.nix
-      	./network/ssh.nix
-      	./services/docker.nix
-        ./services/libvirtd-qemu.nix
-        ./services/lxd.nix
-      	./cloud/tools.nix
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./nixosdotfiles/system/fonts.nix
+      ./nixosdotfiles/system/tty.nix
+      ./nixosdotfiles/system/common.nix
+      ./nixosdotfiles/k8s/tools.nix
+      ./nixosdotfiles/services/virt.nix
+      ./nixosdotfiles/services/containers.nix
+      ./nixosdotfiles/desktop/apps.nix
+      ./nixosdotfiles/users/geir/home.nix
+      /* <home-manager/nixos> */
     ];
 
-  # Use the systemd-boot EFI boot loader.
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.systemd-boot.memtest86.enable = true;
 
-  networking.hostName = "the-sober-counsel";
-  
-  # Set your time zone.
-  time.timeZone = "Europe/Oslo";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Networking
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  # networking.networkmanager.enable = true;
+  networking.hostName = "the-sober-counsel"; # Define your hostname.
   networking.useDHCP = false;
   networking.interfaces.enp4s0.useDHCP = true;
-  networking.interfaces.enp6s0.useDHCP = true;
+  networking.interfaces.enp6s0.useDHCP = false;
   networking.interfaces.wlp5s0.useDHCP = true;
+  networking.interfaces.br0.useDHCP = true;
   networking.bridges = {
   "br0" = {
-    interfaces = [ "enp6s0" ];
+    interfaces = [ "enp4s0" ];
   };
   };
   boot.kernel.sysctl = {
@@ -54,83 +55,84 @@
   ''
     127.0.0.1 localhost
     127.0.0.2 other-localhost
-    127.0.0.1 alpaca.example.com bandicoot.example.com
+    192.168.1.155 dev
+   
 
   '';
 
-  # Enable WireGuard
-  networking.wireguard.interfaces = {
-    wg0 = {
-      ips = [ "10.0.1.3/24" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-      privateKeyFile = "/home/geir/.wireguard/privatekey";
-      peers = [
-        {
-          publicKey = "h+BUJEBEQGHje2dvSjUrNS2Gne+vbv6G4JPzWo3+/3M=";
-          allowedIPs = [ "10.0.1.0/24" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
-          endpoint = "139.162.157.125:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-          persistentKeepalive = 25;
-        }
-      ];
-    };
-  };
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-   console = {
+  i18n.defaultLocale = "en_US.utf8";
+  # Configure console keymap
+  console = {
      font = "Lat2-Terminus16";
      keyMap = "no";
    };
 
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nb_NO.utf8";
+    LC_IDENTIFICATION = "nb_NO.utf8";
+    LC_MEASUREMENT = "nb_NO.utf8";
+    LC_MONETARY = "nb_NO.utf8";
+    LC_NAME = "nb_NO.utf8";
+    LC_NUMERIC = "nb_NO.utf8";
+    LC_PAPER = "nb_NO.utf8";
+    LC_TELEPHONE = "nb_NO.utf8";
+    LC_TIME = "nb_NO.utf8";
+  };
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];
-  
-  # Kernel
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Enable sound.
-  # sound.enable = true;
-  # rtkit is optional but recommended
+  # Enable the Gnome Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "no";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-  #jack.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
 
-  # use the example session manager (no others are packaged yet so this is enabled by default,
-  # no need to redefine it in your config for now)
-  # media-session.enable = true;
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
-  hardware.pulseaudio.enable = false;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.geir = {
-     shell = pkgs.bash;
-     isNormalUser = true;
-     extraGroups = [ "wheel" "docker" "libvirtd" "lxd" "networkmanager" "adbusers" ];
-  };
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    	vim 
-	   emacs
-    	wget
-	 
-];
-  
-  # enable adb android bridge
-  programs.adb.enable = true;
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget curl git
+     htop glances
+     vivaldi vivaldi-widevine vivaldi-ffmpeg-codecs
+  ];
 
-  # Allow unfree
-  nixpkgs.config.allowUnfree = true;
-  
-  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -139,11 +141,21 @@
   #   enableSSHSupport = true;
   # };
 
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  # Enable flatpack 
+  services.flatpak.enable = true;
+  # Enable home-manager
+  # programs.home-manager = {
+  #    enable = true;
+  #   };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -151,7 +163,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
-
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 pushd ~/
 sudo ln -sr nixosdotfiles/ /etc/nixos/
+cp /etc/nixos/configuration.nix nixosdotfiles/hosts/${HOST}.nix
+
 popd
