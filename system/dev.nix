43d714e7bb170fc62a1672ede5ddeefd2cef2ad8
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-12-21 18:06:40 +0100

    started dev.nix to install language servers

--- system/dev.nix
+++ system/dev.nix
@@ -2,7 +2,12 @@
 {
   environment.systemPackages = with pkgs;
   [
+    # general 
     cachix
+
+    # language servers
+    nodePackages.typescript-language-server
+    pythonPackages.jedi-language-server
   ];
 }
 
