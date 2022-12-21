a854ebdfb2930e72a255854c5ebd2c094850318f
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2022-12-21 18:51:49 +0100

    added go and python to dev.nix

--- system/dev.nix
+++ system/dev.nix
@@ -2,13 +2,19 @@
 {
   environment.systemPackages = with pkgs;
   [
-    # general 
+    # general
     cachix
+
+    # languages
+    python3Full
+    go
 
     # language servers
     nodePackages.typescript-language-server
     python3Packages.jedi-language-server
     rnix-lsp
+    gopls
+    
   ];
 }
 
