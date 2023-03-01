a868a0800edc4fae851904e8310b49d463cd9f57
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-03-01 10:20:52 +0100

    updated a lot

--- services/virt.nix
+++ services/virt.nix
@@ -1,12 +1,17 @@
 { pkgs, ... }:
-{  
-  virtualisation.libvirtd.enable = true;
-  programs.dconf.enable = true;
+{
+  #virtualisation.libvirtd.enable = true;
+  #programs.dconf.enable = true;
+
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableWebService = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
 
   environment.systemPackages = with pkgs; [
-    virt-manager
-    libvirt
-    qemu
-    OVMFFull
+   # virt-manager
+   # libvirt
+   # qemu
+   # OVMFFull
+    
   ];
 }
--- users/geir/home.nix
+++ users/geir/home.nix
@@ -14,21 +14,19 @@ nix = {
 };
 
 # user config
-nix.trustedUsers = [ "root" "geir" ];
+nix.settings.trusted-users = [ "root" "geir" ];
 users.users.geir = {
     isNormalUser = true;
     description = "Geir Okkenhaug Jerstad";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker"];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "vboxusers" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
       firefox-wayland
-      vivaldi vivaldi-widevine vivaldi-ffmpeg-codecs
-      thunderbird
-      tilix toot writefreely
+      google-chrome
       gnome.gnome-software gnome.gnome-tweaks gnome.gedit 
       gruvbox-dark-icons-gtk phinger-cursors bibata-cursors beauty-line-icon-theme 
-      python3Full python3Packages.ipython
-      vscode-with-extensions lazygit
+      python3Full
+      vscode-with-extensions
     ];  
   };
 }
