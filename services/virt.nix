cb20641b627beca23c3b8de455662e2cb5216953
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2023-04-19 15:19:54 +0200

    retiring this repo I think

--- services/virt.nix
+++ services/virt.nix
@@ -1,17 +1,13 @@
 { pkgs, ... }:
 {
-  #virtualisation.libvirtd.enable = true;
-  #programs.dconf.enable = true;
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
 
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableWebService = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
 
   environment.systemPackages = with pkgs; [
-   # virt-manager
-   # libvirt
-   # qemu
-   # OVMFFull
-    
+    virt-manager
+    libvirt
+    qemu
+    OVMFFull
   ];
 }
